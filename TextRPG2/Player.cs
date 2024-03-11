9dabe4146f9a9132a26d8417f414d50e0e88ee2e
KJW <kjw9725@gmail.com>
2024-03-11 15:52:57 +0900

    monster, creature player

---
+++ TextRPG2/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG2
+{
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+
+    }
+    class Player : Creature
+    {
+        protected PlayerType type = PlayerType.None;
+
+        protected Player(PlayerType type) : base(CreatureType.Player)
+        {
+            this.type = type;
+        }
+
+
+    }
+
+    class Knight: Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            type = PlayerType.Knight;
+            SetInfo(100, 10);
+        }
+    }
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            type = PlayerType.Archer;
+            SetInfo(75, 12);
+        }
+    }
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            type = PlayerType.Mage;
+            SetInfo(50, 15);
+        }
+    }
+}
--- TextRPG2/Program.cs
+++ TextRPG2/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Player player = new Knight();
+            Monster monster = new Orc();
+
+            int damage = player.GetAttack();
+            monster.OnDamaged(damage);
         }
     }
 }
